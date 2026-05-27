5c9725b8e0a070859a5b7dd0373bcda12358495a
Satyam202217b <202217b4541@wilp.bits-pilani.ac.in>
2026-05-27 11:26:15 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -72,21 +72,21 @@ async function sendMessage() {
        RASA API CALL
     ========================= */
  
-    const response = await fetch(
-      "http://localhost:5005/webhooks/rest/webhook",
-      {
-        method: "POST",
- 
-        headers: {
-          "Content-Type": "application/json"
-        },
- 
-        body: JSON.stringify({
-          sender: "user",
-          message: message
-        })
-      }
-    );
+      const response = await fetch(
+  "https://ai-g1jw.onrender.com/webhooks/rest/webhook",
+  {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json"
+    },
+
+    body: JSON.stringify({
+      sender: "user",
+      message: message
+    })
+  }
+);
  
     const data = await response.json();
  
